9fed93de1c92b48ac2661ab3077184182adcc025
Suganthi E <suganthi2210@gmail.com>
2026-08-01 13:12:13 +0530

    Updated API URL for EC2 deployment

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://localhost:7218/api"
+    baseURL: import.meta.env.VITE_API_URL
 });
 api.interceptors.request.use(
     config => {
