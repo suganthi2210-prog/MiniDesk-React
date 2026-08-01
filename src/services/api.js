f09b81b9933086c88f718c6abd55f11f48ba4c29
Suganthi E <suganthi2210@gmail.com>
2026-08-01 18:21:08 +0530

    Error fixed

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    //baseURL: import.meta.env.VITE_API_URL
-    baseURL: "https://localhost:7218/api"
+    baseURL: import.meta.env.VITE_API_URL
+    //baseURL: "https://localhost:7218/api"
 });
 api.interceptors.request.use(
     config => {
